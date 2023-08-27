f716123d48587152f784a3388fd3e13d9166d399
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-08-27 05:06:18 -0400

    Added a function to render the posts

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-/**
- Challenge:
-
- With the 5 blog post objects, display the `title` and `body`
-properties of the first 5 posts on the browser page.
- 
- Hints: 
- * Create a `div` in the HTML file to store these items
- * Loop over the items creating a string of HTML elements you 
-   can then put into the div with `innerHTML`
- */
-
 const postsEl = document.getElementById('posts');
+const postBtn = document.getElementById('post-btn');
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        const postsArray = data.slice(0, 5);
-        let html = ``;
-        for (let post of postsArray){
-            html += `
+let postsArray = [];
+
+function renderPosts() {
+    let html = ``;
+    for (let post of postsArray) {
+        html += `
                 <h2>${post.title}</h2>
                 <p>${post.body}</p>
                 <hr />
             `
-        }
-        postsEl.innerHTML = html;
+    }
+    postsEl.innerHTML = html;
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        postsArray = data.slice(0, 5);
+        renderPosts();
     })
+
+
+
+postBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    const newPost = {};
+    newPost.title = document.getElementById('post-title').value;
+    newPost.body = document.getElementById('post-body').value;
+    console.log(newPost);
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            postsArray.unshift(post);
+            renderPosts();
+        })
+})
